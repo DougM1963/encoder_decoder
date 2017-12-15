90e12358898fc013f66e2ee476d58aaad03c9c02
dougm1963 <dougmeador1963@gmail.com>
2017-12-15 12:22:06 -0500

    Finished project

--- encoder.rb
+++ encoder.rb
@@ -3,17 +3,17 @@
     puts "input message to encode or decode."
     message_used = gets.chomp
     message_array = message_used.split("")
-    #puts message_array
+   # print message_array
 
-    alphabet1 = {"a" => n, "b" => o, "c" => p, "d" => q, "e" => r , "f" => s,
-    "g" => t, "h" => u, "i" => v, "j" => w, "k" => x, "l" => y, "m" => z,
-    "n" => a, "o" => b, "p" => c, "q" => d, "r" => e, "s" => f, "t" => g, 
-    "u" => h, "v" => i, "w" => j, "x" => k, "y" => l, "z" => m, " " => " "}
+    alphabet1 = {"a" => "n", "b" => "o", "c" => "p", "d" => "q", "e" => "r" , "f" => "s",
+    "g" => "t", "h" => "u", "i" => "v", "j" => "w", "k" => "x", "l" => "y", "m" => "z",
+    "n" => "a", "o" => "b", "p" => "c", "q" => "d", "r" => "e", "s" => "f", "t" => "g", 
+    "u" => "h", "v" => "i", "w" => "j", "x" => "k", "y" => "l", "z" => "m", " " => " "}
 
     counter = 0
     message_array.each do 
-    #encoded_message = encoded_message + alphabet_hash.fetch(alphabet1)
-    print message_array[counter]," ", alphabet1[message_array [counter]]
+    #encoded_message = encoded_message + alphabet1.fetch(alphabet1)
+    print message_array[counter]," ", alphabet1[message_array[counter]], "\n"
     counter = counter + 1
     end
 
