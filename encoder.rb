c49bda1f74d5a07f7de83e2ee163c02e728db2db
dougm1963 <dougmeador1963@gmail.com>
2017-12-14 20:50:35 -0500

    printing out some need function

--- encoder.rb
+++ encoder.rb
@@ -1,19 +1,29 @@
-puts "Do you want to encode or decode?"
-code = gets.chomp
+    puts "Do you want to encode or decode?"
+    code = gets.chomp
+
+    puts "input message to encode or decode."
+    message_used = gets.chomp
+    message_used.downcase!
+
+    message_array = message_used.split("")
+    puts message_array
+
+def encoded_message(message_array)
+	message_array.each do |alphabet_hash|
+
+end
+
+
+    alphabet_hash = {"a" => m, "b" => n, "c" => o, "d" => p, "e" => q , "f" => r,
+    "g" => s, "h" => t, "i" => u, "j" => v, "k" => w, "l" => x, "m" => y,
+    "n" => z, "o" => a, "p" => b, "q" => c, "r" => d, "s" => e, "t" => f, 
+    "u" => g, "v" => h, "w" => i, "x" => j, "y" => k, "z" => l}
+
+    output_message = ""
 
 
 
 
 
 
-
-
-puts "input message to code."
-message = gets.chomp
-
-
-alphabet = {"a" =>m, "b" => n, "c" => o, "d" => p, "e" => q , "f" => r,
- "g" => s, "h" => t, "i" => u, "j" => v, "k" => w, "l" => x, "m" => y,
- "n" => z, "o" => a, "p" => b, "q" => c, "r" => d, "s" => e, "t" => f, 
- "u" => g, "v" => h, "w" => i, "x" => j, "y" => k, "z" => l}
-
+end
