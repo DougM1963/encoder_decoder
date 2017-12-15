8bfa4332ba1a35c7728357759c3b3594a17b606a
dougm1963 <dougmeador1963@gmail.com>
2017-12-14 19:49:07 -0500

    start of encoder

---
+++ encoder.rb
@@ -0,0 +1,19 @@
+puts "Do you want to encode or decode?"
+code = gets.chomp
+
+
+
+
+
+
+
+
+puts "input message to code."
+message = gets.chomp
+
+
+alphabet = {"a" =>m, "b" => n, "c" => o, "d" => p, "e" => q , "f" => r,
+ "g" => s, "h" => t, "i" => u, "j" => v, "k" => w, "l" => x, "m" => y,
+ "n" => z, "o" => a, "p" => b, "q" => c, "r" => d, "s" => e, "t" => f, 
+ "u" => g, "v" => h, "w" => i, "x" => j, "y" => k, "z" => l}
+
